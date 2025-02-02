5d454a51868634ecc64941a973ec2f1f102ff054
DmitrySummer <dielsunsummer@yahoo.com>
2025-02-02 11:20:23 +0300

    new

--- file/file.go
+++ file/file.go
@@ -11,18 +11,34 @@ func WriteFile(content []byte, name string) error {
 	if !json.Valid(content) {
 		return fmt.Errorf("Некорректный JSON, ошибка записи")
 	}
+	var existingData []map[string]interface{}
+	if data, err := os.ReadFile(name); err == nil && len(data) > 0 {
+		json.Unmarshal(data, &existingData)
+	}
+	var newData map[string]interface{}
+	if err := json.Unmarshal(content, &newData); err != nil {
+		return fmt.Errorf("Ошибка декодирования JSON: %v", err)
+	}
 
-	file, err := os.Create(name)
+	existingData = append(existingData, newData)
+
+	updatedData, err := json.MarshalIndent(existingData, "", "  ")
+	if err != nil {
+		return fmt.Errorf("Ошибка кодирования JSON: %v", err)
+	}
+
+	file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	_, err = file.Write(content)
+	file.Truncate(0)
+	_, err = file.Write(updatedData)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Файл записался успешно")
+	fmt.Println("Данные успешно добавлены в", name)
 	return nil
 }
--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,10 @@
 package storage
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 // Функция для прочтения файла
 func ReadFile(name string) ([]byte, error) {
@@ -11,11 +15,25 @@ func ReadFile(name string) ([]byte, error) {
 	return data, nil
 }
 
-// Функция по сохранению файла
-func SaveFile(name string, content []byte) error {
-	err := os.WriteFile(name, content, 0644)
-	if err != nil {
-		return err
+// Функция по сохранению файла, если файл уже существует, то добавит запись уже в имеющийся
+func SaveFile(name string, newData map[string]interface{}) error {
+	var existingData []map[string]interface{}
+	file, err := os.ReadFile(name)
+	if err == nil && len(file) > 0 {
+		if err := json.Unmarshal(file, &existingData); err != nil {
+			return fmt.Errorf("ошибка декодирования JSON: %v", err)
+		}
 	}
+	existingData = append(existingData, newData)
+	updatedData, err := json.Marshal(existingData)
+	if err != nil {
+		return fmt.Errorf("ошибка кодирования JSON: %v", err)
+	}
+
+	if err := os.WriteFile(name, updatedData, 0); err != nil {
+		return fmt.Errorf("ошибка записи в файл: %v", err)
+	}
+
+	fmt.Println("Данные успешно добавлены в", name)
 	return nil
 }
